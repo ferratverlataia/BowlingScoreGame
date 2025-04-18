7f36d7b583539af6d7ed9599012b4f7768b96128
ifv65 <ivanferrat65@gmail.com>
2025-04-18 09:38:31 -0600

    Ivan-0003
    -fixed spares functioning as strikes
    -fixed strikes not adding properly

--- Source/BowlingScoreGame/Private/Core/BowlingScoreModel.cpp
+++ Source/BowlingScoreGame/Private/Core/BowlingScoreModel.cpp
@@ -68,6 +68,7 @@ void ABowlingScoreModel::CalculateScore()
 			break;
 		case EFrameTypeScore::Strike:
 			//strikes edge cases when we are on last 3 frames
+			//when its the last frame
 			if (FrameIndex==NumberOfRounds)
 			{
 				FrameScores[FrameIndex]= BowlingFrames[FrameIndex]->AddRoundsScores();
@@ -98,11 +99,11 @@ void ABowlingScoreModel::CalculateScore()
 			}
 			else
 			{
-				FrameScores[FrameIndex]=BowlingFrames[FrameIndex]->AddRoundsScores();
+				FrameScores[FrameIndex]=BowlingFrames[FrameIndex]->AddRoundsScores()+BowlingFrames[FrameIndex+1]->AddRoundsScores();
 			}
 			break;
 		}
-		//We add to total score the frame score and then we broadcast the change so that any system can get the score of a frame
+		//We add to total score the frame score, and then we broadcast the change so that any system can get the score of a frame
 		TotalScore+=FrameScores[FrameIndex];
 		OnFrameScoreChanged.Broadcast(FrameIndex,FrameScores[FrameIndex],TotalScore);
 		FrameIndex++;
